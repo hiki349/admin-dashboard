131cb7f0e218b52768ce711be4e492fd0e83619b
hiki349 <vladyakima@gmail.com>
2023-11-06 06:12:42 +0200

    create FAQ

--- src/scenes/faq/FAQ.tsx
+++ src/scenes/faq/FAQ.tsx
@@ -1,1 +1,97 @@
-export const FAQ = () => <></>
+import { Box, Typography, useTheme } from "@mui/material";
+import { Header } from "../../components";
+import { Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
+import { ExpandMore } from "@mui/icons-material";
+import { tokens } from "../../utils";
+
+export const FAQ = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m="20px">
+      <Header title="FAQ" subtitle="Frequenty Asked Questions Page" />
+
+      <Accordion
+        sx={{ backgroundColor: colors.primary["400"] }}
+        defaultExpanded
+      >
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            An Important Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        sx={{ backgroundColor: colors.primary["400"] }}
+        defaultExpanded
+      >
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Another Important Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        sx={{ backgroundColor: colors.primary["400"] }}
+        defaultExpanded
+      >
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Your Favorite Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        sx={{ backgroundColor: colors.primary["400"] }}
+        defaultExpanded
+      >
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Some Random Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        sx={{ backgroundColor: colors.primary["400"] }}
+        defaultExpanded
+      >
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            The Final Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
+  );
+};
