9d33babe06cde48e6ac37aee87185cbeff6991c2
hiki349 <vladyakima@gmail.com>
2023-11-06 08:00:25 +0200

    create Bar Chart

--- src/scenes/bar/Bar.tsx
+++ src/scenes/bar/Bar.tsx
@@ -1,1 +1,13 @@
-export const Bar = () => <></>
+import { Box } from "@mui/material";
+import { BarChart, Header } from "../../components";
+
+export const Bar = () => {
+  return (
+    <Box m="20px">
+      <Header title="BAR CHART" subtitle="Simple Bar Chart" />
+      <Box height="75vh">
+        <BarChart />
+      </Box>
+    </Box>
+  );
+};
